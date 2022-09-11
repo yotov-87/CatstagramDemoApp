5de3e511c1ff0fd667aeea0b624548875c5af3b1
dimitar.yotov <dimitar.yotov@blu11.com>
2022-09-11 18:18:54 +0300

    created extension method for swagger

--- CatstagramDemoApp.Server/CatstagramDemoApp.Server/Program.cs
+++ CatstagramDemoApp.Server/CatstagramDemoApp.Server/Program.cs
@@ -1,13 +1,4 @@
-using CatstagramDemoApp.Server;
-using CatstagramDemoApp.Server.Data;
-using CatstagramDemoApp.Server.Data.Models;
 using CatstagramDemoApp.Server.Infrastructure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,11 +30,7 @@ if (app.Environment.IsDevelopment()) {
 //}
 
 app.UseHttpsRedirection()
-    .UseSwagger()
-    .UseSwaggerUI( options => {
-        options.SwaggerEndpoint("swagger/v1/swagger.json", "My Catstagram API v1");
-        options.RoutePrefix = string.Empty;
-    })
+    .UseSwaggerUI()
     .UseRouting()
     .UseCors(x => x
                 .AllowAnyOrigin()
