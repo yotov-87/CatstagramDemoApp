0ccd360672b4782838d3754694b9cdde8a4e3f6a
dimitar.yotov <dimitar.yotov@blu11.com>
2022-09-17 18:33:28 +0300

    added endpoint to receive all cats created by the user

--- CatstagramDemoApp.Server/CatstagramDemoApp.Server/Features/Cats/ICatService.cs
+++ CatstagramDemoApp.Server/CatstagramDemoApp.Server/Features/Cats/ICatService.cs
@@ -1,5 +1,8 @@
-﻿namespace CatstagramDemoApp.Server.Features.Cats {
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatstagramDemoApp.Server.Features.Cats {
     public interface ICatService {
-        Task<int> Create(string imageUrl, string description, string userId);
+        public Task<int> Create(string imageUrl, string description, string userId);
+        public Task<IEnumerable<CatListingResponseModel>> ByUser(string userId);
     }
 }
